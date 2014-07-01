2f0650087dc41ebbc0eb9cef0a33911fc4f1763a
Stefan Dentro <sd11@sanger.ac.uk>
2014-07-01 17:02:14 +0100

    Fixed bugs when loading tree phase from disk

--- dp_tree_based/RunTreeBasedDP.R
+++ dp_tree_based/RunTreeBasedDP.R
@@ -170,12 +170,14 @@ RunTreeBasedDP<-function(mutCount, WTCount, cellularity = rep(1,ncol(mutCount)),
   
   } else {
     ## Load the data
+    setwd(outdir)
     load(file=paste("",samplename,"_trees_iters",no.iters,".Rdata",sep=""))
-    node.assignments = read.table(paste("node_assignments_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t",row.names=F,quote=F)
-    alphas = read.table(paste("alphas_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t",row.names=F,quote=F)
-    lambdas = read.table(lambdas,paste("lambdas_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t",row.names=F,quote=F)
-    gammas = read.table(gammas,paste("gammas_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t",row.names=F,quote=F)
-    likelihoods = read.table(paste("likelihoods_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t",row.names=F,quote=F)
+    node.assignments = read.table(paste("node_assignments_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t")
+    alphas = read.table(paste("alphas_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t")
+    lambdas = read.table(paste("lambdas_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t")
+    gammas = read.table(paste("gammas_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t")
+    likelihoods = read.table(paste("likelihoods_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t")
+    binned.node.assignments = read.table(paste("aggregated_node_assignments_",samplename,"_",no.iters,"iters.txt",sep=""),sep="\t")
   } 
 	
   if (is.na(phase) | phase == 'cons') {
