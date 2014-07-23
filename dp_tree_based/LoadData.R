efb15fa0ed54fa97b5b87cf0b018ccee08d81fb5
Stefan Dentro <sd11@sanger.ac.uk>
2014-07-23 11:53:54 +0100

    Fixing bug where subsamples[[run]] was still called. Is now replaced by the list_of_data_files variable that is given in the input

--- dp_tree_based/LoadData.R
+++ dp_tree_based/LoadData.R
@@ -1,4 +1,4 @@
-load.data <- function(datpath, samplename, list_of_data_files, Chromosome, WT.count, mut.count, subclonal.CN, no.chrs.bearing.mut, data_file_suffix) {
+load.data <- function(datpath, samplename, list_of_data_files, cellularity, Chromosome, WT.count, mut.count, subclonal.CN, no.chrs.bearing.mut, data_file_suffix) {
   # Takes:
   # - An array of data file names to be read in
   # - WT.count, the colname of the column in the data files that contains the number of WT reads
@@ -10,22 +10,26 @@ load.data <- function(datpath, samplename, list_of_data_files, Chromosome, WT.co
     data[[s]] = read.table(paste(datpath,samplename,list_of_data_files[s],data_file_suffix,sep=""),header=T,sep="\t")
     data[[s]] = data[[s]][data[[s]][,Chromosome] %in% 1:22,]
   }
+
+  no.subsamples = length(list_of_data_files)
   
   WTCount = array(0,c(nrow(data[[1]]),no.subsamples))
   mutCount = array(0,c(nrow(data[[1]]),no.subsamples))
   totalCopyNumber = array(0,dim(WTCount))
   copyNumberAdjustment = array(0,dim(WTCount))
   non.deleted.muts = vector(mode="logical",length=nrow(data[[1]]))
-  for(s in 1:length(subsamples[[run]])){
+  mutation.copy.number = array(NA,dim(WTCount))
+  for(s in 1:length(list_of_data_files)){
     WTCount[,s] = as.numeric(data[[s]][,WT.count])
     mutCount[,s] = as.numeric(data[[s]][,mut.count])
     totalCopyNumber[,s] = as.numeric(data[[s]][,subclonal.CN])
     copyNumberAdjustment[,s] = as.numeric(data[[s]][,no.chrs.bearing.mut])
     non.deleted.muts[data[[s]]$no.chrs.bearing.mut>0]=T
+    mutation.copy.number[,s] = as.numeric(data[[s]]$mutation.copy.number)
   }
   
   kappa = array(1,dim(mutCount))
-  for(i in 1:length(subsamples[[run]])){
+  for(i in 1:length(list_of_data_files)){
     #multiply by no.chrs.bearing.mut, so that kappa is the fraction of reads required for fully clonal mutations, rather than mutation at MCN = 1
     kappa[,i] = mutationCopyNumberToMutationBurden(1,data[[i]]$subclonal.CN,cellularity[i]) * data[[i]]$no.chrs.bearing.mut
   }
@@ -38,15 +42,18 @@ load.data <- function(datpath, samplename, list_of_data_files, Chromosome, WT.co
   not.there.cna = sapply(1:no.muts, FUN=function(i, dat1) { sum(is.na(dat1[i,]))>0}, dat1=copyNumberAdjustment)
   not.there.kappa = sapply(1:no.muts, FUN=function(i, dat1) { sum(is.na(dat1[i,]))>0}, dat1=kappa)
   # Remove those mutations that have no coverage. These cause for trouble lateron. A better solution should be found for these.
-  no.coverage = sapply(1:no.muts, FUN=function(i, dat1) { sum(dat1[i,]==0)>0}, dat1=WTCount+mutCount)
-  select = !(not.there.wt | not.there.mut | not.there.cn | not.there.cna | not.there.kappa | no.coverage)
+  not.coverage = sapply(1:no.muts, FUN=function(i, dat1) { any(dat1[i,]==0)}, dat1=WTCount+mutCount)
+  not.cna = sapply(1:no.muts, FUN=function(i, dat1) { any(dat1[i,]==0)}, dat1=copyNumberAdjustment)
+  
+  select = !(not.there.wt | not.there.mut | not.there.cn | not.there.cna | not.there.kappa | not.coverage | not.cna)
   WTCount = WTCount[select,]
   mutCount = mutCount[select,]
   totalCopyNumber = totalCopyNumber[select,]
   copyNumberAdjustment = copyNumberAdjustment[select,]
   non.deleted.muts = non.deleted.muts[select]
   kappa = kappa[select,]
+  mutation.copy.number = mutation.copy.number[select,]
   print(paste("Removed",no.muts-nrow(WTCount), "mutations with missing data"))
   
-  return(list(WTCount=WTCount, mutCount=mutCount, totalCopyNumber=totalCopyNumber, copyNumberAdjustment=copyNumberAdjustment, non.deleted.muts=non.deleted.muts, kappa=kappa))
+  return(list(WTCount=WTCount, mutCount=mutCount, totalCopyNumber=totalCopyNumber, copyNumberAdjustment=copyNumberAdjustment, non.deleted.muts=non.deleted.muts, kappa=kappa, mutation.copy.number=mutation.copy.number))
 }
