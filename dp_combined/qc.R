d66cdf540d0b57b50f3a856deed33ec179c8ce9e
Stefan Dentro <sd11@sanger.ac.uk>
2014-11-06 16:47:01 +0000

    Checkinf whether a file actually exists before reading

--- dp_combined/qc.R
+++ dp_combined/qc.R
@@ -148,6 +148,8 @@ for (samplename in unique(sample2purity$sample)) {
   datafiles = sample2purity[sample2purity$sample==samplename,]$datafile
   subsamples = sample2purity[sample2purity$sample==samplename,]$subsample
   cellularity = sample2purity[sample2purity$sample==samplename,]$cellularity
-  dataset = load.data(datpath,"",datafiles, cellularity=cellularity, Chromosome="chr", position="pos", WT.count="WT.count", mut.count="mut.count", subclonal.CN="subclonal.CN", no.chrs.bearing.mut="no.chrs.bearing.mut", mutation.copy.number="mutation.copy.number", subclonal.fraction="subclonal.fraction", data_file_suffix="")
-  createQCDocument(dataset, samplename, subsamples, outpath, cellularity)
+  if (file.exists(paste(datpath,datafiles, sep=""))) {
+    dataset = load.data(datpath,"",datafiles, cellularity=cellularity, Chromosome="chr", position="pos", WT.count="WT.count", mut.count="mut.count", subclonal.CN="subclonal.CN", no.chrs.bearing.mut="no.chrs.bearing.mut", mutation.copy.number="mutation.copy.number", subclonal.fraction="subclonal.fraction", data_file_suffix="")
+    createQCDocument(dataset, samplename, subsamples, outpath, cellularity)
+  }
 }
