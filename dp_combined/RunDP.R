407a8713cf3d354ab8eebf9015e0af737eab30da
Stefan Dentro <sd11@sanger.ac.uk>
2014-11-15 21:18:52 +0000

    Both nD and treebased clusterings now return clustering results that are saved as bestConsensusAssignments and bestConsensusTree (if possible)

--- dp_combined/RunDP.R
+++ dp_combined/RunDP.R
@@ -1,21 +1,21 @@
 source("DirichletProcessClustering.R")
 
-RunDP <- function(analysis_type, dataset, samplename, subsamples, no.iters, no.iters.burn.in, outdir, conc_param, cluster_conc, resort.mutations, parallel, blockid, no.of.blocks, annotation=vector(mode="character",length=nrow(dataset$mutCount)), init.alpha=0.01, shrinkage.threshold=0.1, remove.node.frequency=NA, remove.branch.frequency=NA)
+RunDP <- function(analysis_type, dataset, samplename, subsamples, no.iters, no.iters.burn.in, outdir, conc_param, cluster_conc, resort.mutations, parallel, blockid, no.of.blocks, annotation=vector(mode="character",length=nrow(dataset$mutCount)), init.alpha=0.01, shrinkage.threshold=0.1, remove.node.frequency=NA, remove.branch.frequency=NA, bin.size=NA) {
   # Pick the analysis to run
   if (analysis_type == 'nd_dp') {
-    DirichletProcessClustering(mutCount=dataset$mutCount, 
-                        WTCount=dataset$WTCount, 
-                        no.iters=no.iters, 
-                        no.iters.burn.in=no.iters.burn.in, 
-                        cellularity=cellularity, 
-                        totalCopyNumber=dataset$totalCopyNumber, 
-                        mutation.copy.number=dataset$mutation.copy.number,
-                        copyNumberAdjustment=dataset$copyNumberAdjustment, 
-                        samplename=samplename, 
-			                  subsamplesrun=subsamples,
-                        output_folder=outdir, 
-                        conc_param=conc_param, 
-                        cluster_conc=cluster_conc)
+    clustering = DirichletProcessClustering(mutCount=dataset$mutCount, 
+                                            WTCount=dataset$WTCount, 
+                                            no.iters=no.iters, 
+                                            no.iters.burn.in=no.iters.burn.in, 
+                                            cellularity=cellularity, 
+                                            totalCopyNumber=dataset$totalCopyNumber, 
+                                            mutation.copy.number=dataset$mutation.copy.number,
+                                            copyNumberAdjustment=dataset$copyNumberAdjustment, 
+                                            samplename=samplename, 
+                    			                  subsamplesrun=subsamples,
+                                            output_folder=outdir, 
+                                            conc_param=conc_param, 
+                                            cluster_conc=cluster_conc)
   } else if(analysis_type == "tree_dp" | analysis_type == 'tree' | analysis_type == 'cons') {
     if(is.na(bin.size)){
       outdir = paste(samplename,"_treeBasedDirichletProcessOutputs_noIters",no.iters,"_burnin",no.iters.burn.in,sep="")
@@ -23,26 +23,40 @@ RunDP <- function(analysis_type, dataset, samplename, subsamples, no.iters, no.i
     }else{
       outdir = paste(samplename,"_treeBasedDirichletProcessOutputs_noIters",no.iters,"_binSize",bin.size,"_burnin",no.iters.burn.in,sep="")
     }
-    TreeBasedDP(mutCount=dataset$mutCount,
-                   WTCount=dataset$WTCount,
-                   kappa=dataset$kappa, 
-                   samplename = samplename, 
-                   subsamplenames = subsamples,
-                   no.iters=no.iters,
-                   no.iters.burn.in=no.iters.burn.in,
-                   cellularity=cellularity,
-                   bin.size = bin.size, 
-                   resort.mutations = resort.mutations, 
-                   outdir = outdir, 
-                   parallel=parallel, 
-                   phase=analysis_type, 
-                   blockid=blockid, 
-                   no.of.blocks=no.of.blocks,
-                   remove.node.frequency=remove.node.frequency, 
-                   remove.branch.frequency=remove.branch.frequency,
-                   annotation=annotation,
-                   init.alpha=init.alpha, 
-                   shrinkage.threshold=shrinkage.threshold)
+    clustering = TreeBasedDP(mutCount=dataset$mutCount,
+                             WTCount=dataset$WTCount,
+                             kappa=dataset$kappa, 
+                             samplename=samplename, 
+                             subsamplenames=subsamples,
+                             no.iters=no.iters,
+                             no.iters.burn.in=no.iters.burn.in,
+                             cellularity=cellularity,
+                             bin.size=bin.size, 
+                             resort.mutations=resort.mutations, 
+                             outdir=outdir, 
+                             parallel=parallel, 
+                             phase=analysis_type, 
+                             blockid=blockid, 
+                             no.of.blocks=no.of.blocks,
+                             remove.node.frequency=remove.node.frequency, 
+                             remove.branch.frequency=remove.branch.frequency,
+                             annotation=annotation,
+                             init.alpha=init.alpha, 
+                             shrinkage.threshold=shrinkage.threshold)
   } else {
     print(paste("Unknown type of analysis",analysis_type))
+    q(save="no", status=1)
   }
+
+  if (analysis_type != 'tree') {
+    # Write final output
+    output = cbind(dataset$chromosome[,1], dataset$position[,1]-1, dataset$position[,1], clustering$best.node.assignments, clustering$best.assignment.likelihoods)
+    save(file=paste(samplename, "_", no.iters, "iters_", no.iters.burn.in, "burnin_bestConsensusResults.RData", sep=""), output, clustering, samplename, outdir, no.iters, no.iters.burn.in)
+    colnames(output) = c("chr", "start", "end", "cluster", "likelihood")
+    write.table(output, file=paste(samplename, "_", no.iters, "iters_", no.iters.burn.in, "burnin_bestConsensusAssignments.bed", sep=""), quote=F, row.names=F, sep="\t")
+    # If tree based analysis, also save the tree
+    if (analysis_type != 'nd_dp') {
+      write.table(clustering$best.tree, file=paste(samplename, "_", no.iters, "iters_", no.iters.burn.in, "burnin_bestConsensusTree.txt", sep=""), quote=F, row.names=F, sep="\t")
+    }
+  }
+}
